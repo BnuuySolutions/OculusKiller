291eb1f6b8b7c6f29490fa5ef212a85a80f7d7c2
SMartin <serjimartsergio@gmail.com>
2022-04-26 01:06:53 +0200

    Added logger

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using Serilog;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -10,27 +11,44 @@ namespace OculusKiller
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(
+                    path: $"OculusKiller-{DateTime.Now:yyyyMMdd}.log",
+                    fileSizeLimitBytes: 1000 * 500, // 500 KB aprox.
+                    retainedFileCountLimit: 2)
+                .CreateLogger();
+
+            Log.Information("Initialize OculusKiller...");
+
             try
             {
                 var steamPath = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam", "SteamPath", null).ToString();
-                if (!string.IsNullOrEmpty(steamPath))
+                if (string.IsNullOrEmpty(steamPath))
                 {
-                    steamPath = Path.GetFullPath(steamPath);
+                    throw new DirectoryNotFoundException("Steam installation not found. Searched at registry => HKEY_CURRENT_USER\"Software\"Valve\"Steam");
+                }
 
-                    // Going to assume SteamVR is in the default steamapps path, this could be bad.
-                    var binaryPath = Path.Combine(steamPath, @"steamapps\common\SteamVR\bin\win64");
-                    var vrStartupPath = Path.Combine(binaryPath, "vrstartup.exe");
-                    if (Directory.Exists(binaryPath) && File.Exists(vrStartupPath))
-                    {
-                        var vrStartupProcess = Process.Start(vrStartupPath);
-                        vrStartupProcess.WaitForExit();
-                    }
-                    else
-                        MessageBox.Show("Couldn't find SteamVR! (Did you install it and run it once?)");
+                steamPath = Path.GetFullPath(steamPath);
+
+                // Going to assume SteamVR is in the default steamapps path, this could be bad.
+                var binaryPath = Path.Combine(steamPath, @"steamapps\common\SteamVR\bin\win64");
+                var vrStartupPath = Path.Combine(binaryPath, "vrstartup.exe");
+                if (Directory.Exists(binaryPath) && File.Exists(vrStartupPath))
+                {
+                    var vrStartupProcess = Process.Start(vrStartupPath);
+                    vrStartupProcess.WaitForExit();
+                    Log.Information("...OculusKiller run successfully.");
+                    Log.CloseAndFlush();
+                }
+                else
+                {
+                    Log.Error($"Couldn't find SteamVR at {vrStartupPath}");
+                    MessageBox.Show($"Couldn't find SteamVR! (Did you install it and run it once?)");
                 }
             }
             catch (Exception ex)
             {
+                Log.Error(ex, $"An exception occured.");
                 MessageBox.Show($"An exception occured while attempting to find SteamVR! (Did you install it and run it once?)\n\nMessage: {ex}");
             }
         }
