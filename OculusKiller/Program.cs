1a01b7177c6ef9a80f49dc50cd4a3a97f5ac5f05
Hyrum Nantais <hyrumaknantais@gmail.com>
2022-05-12 09:19:35 -0400

    Try using steams browser protocol instead

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Windows.Forms;
 
 namespace OculusKiller
@@ -12,27 +10,8 @@ namespace OculusKiller
         {
             try
             {
-                RegistryKey steamVRKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 250820");
-
-                if (steamVRKey != null)
-                {
-                    string programPath = steamVRKey.GetValue("InstallLocation").ToString();
-                    string vrStartupPath = Path.Combine(programPath, @"bin\win64\vrstartup.exe");
-
-                    if (File.Exists(vrStartupPath))
-                    {
-                        Process vrStartupProcess = Process.Start(vrStartupPath);
-                        vrStartupProcess.WaitForExit();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Unable to find vrstartup executable within SteamVR installation directory.");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Couldn't find SteamVR! (Did you install it and run it once?)");
-                }
+                Process vrStartupProcess = Process.Start("steam://run/250820");
+                vrStartupProcess.WaitForExit();
             }
             catch (Exception ex)
             {
