ef1107669a2c08adb40353a390a68e7fddd81430
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2022-05-16 15:14:01 -0700

    Use gameloop.vdf to parse SteamVR location

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using Gameloop.Vdf;
+using Gameloop.Vdf.Linq;
 
 namespace OculusKiller
 {
@@ -12,26 +14,43 @@ namespace OculusKiller
         {
             try
             {
-                RegistryKey steamVrKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 250820");
-                if (steamVrKey != null)
+                RegistryKey steamKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Valve\Steam");
+                if (steamKey == null)
                 {
-                    string installLocation = steamVrKey.GetValue("InstallLocation").ToString();
-                    string vrStartupPath = Path.Combine(installLocation, @"bin\win64\vrstartup.exe");
-
-                    if (File.Exists(vrStartupPath))
-                    {
-                        Process vrStartupProcess = Process.Start(vrStartupPath);
-                        vrStartupProcess.WaitForExit();
-                    }
-                    else
-                        MessageBox.Show("Unable to find vrstartup executable within SteamVR installation directory.");
+                    MessageBox.Show("Steam is not installed!");
+                    return;
                 }
-                else
-                    MessageBox.Show("Couldn't find SteamVR! (Did you install it and run it once?)");
+
+                string steamLocation = steamKey.GetValue("SteamPath").ToString();
+                string libraryConfigLocation = Path.Combine(steamLocation, @"steamapps\libraryfolders.vdf");
+                if (!File.Exists(libraryConfigLocation))
+                {
+                    MessageBox.Show("Steam library config does not exist!");
+                }
+
+                VProperty libraryConfig = VdfConvert.Deserialize(File.ReadAllText(libraryConfigLocation));
+                foreach (VProperty child in libraryConfig.Value.Children())
+                {
+                    if (child.Value["apps"]["250820"] != null)
+                    {
+                        string steamVrLibPath = child.Value["path"].ToString();
+                        string vrStartupPath = Path.Combine(steamVrLibPath, @"steamapps\common\SteamVR\bin\win64\vrstartup.exe");
+
+                        if (File.Exists(vrStartupPath))
+                        {
+                            Process vrStartupProcess = Process.Start(vrStartupPath);
+                            vrStartupProcess.WaitForExit();
+                        }
+                        else
+                            MessageBox.Show("SteamVR does not exist in installation directory.");
+                        return;
+                    }
+                }
+                MessageBox.Show("SteamVR installation not found in library.");
             }
             catch (Exception e)
             {
-                MessageBox.Show($"An exception occured while attempting to find SteamVR! (Did you install it and run it once?)\n\nMessage: {e}");
+                MessageBox.Show($"An exception occured while attempting to find SteamVR!\n\nMessage: {e}");
             }
         }
     }
