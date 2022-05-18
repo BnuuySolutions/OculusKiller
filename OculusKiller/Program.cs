7d23cca2a44685bd7a329aa1cdcf0c2370d0a363
UnusualNorm <40722525+UnusualNorm@users.noreply.github.com>
2022-05-18 16:48:11 -0700

    Use OpenVR vrpaths file to parse SteamVR location (#38)
    * Use gameloop.vdf to parse SteamVR location
    
    * Use openvrpath config instead
    
    Co-authored-by: Hyrum Nantais <hyrumaknantais@gmail.com>

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using System.Web.Script.Serialization;
 
 namespace OculusKiller
 {
@@ -12,26 +12,28 @@ namespace OculusKiller
         {
             try
             {
-                RegistryKey steamVrKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 250820");
-                if (steamVrKey != null)
+                string openVrPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"openvr\openvrpaths.vrpath");
+                if (File.Exists(openVrPath))
                 {
-                    string installLocation = steamVrKey.GetValue("InstallLocation").ToString();
-                    string vrStartupPath = Path.Combine(installLocation, @"bin\win64\vrstartup.exe");
+                    var jss = new JavaScriptSerializer();
+                    string jsonString = File.ReadAllText(openVrPath);
+                    dynamic steamVrPath = jss.DeserializeObject(jsonString);
 
+                    string vrStartupPath = Path.Combine(steamVrPath["runtime"][0].ToString(), @"bin\win64\vrstartup.exe");
                     if (File.Exists(vrStartupPath))
                     {
                         Process vrStartupProcess = Process.Start(vrStartupPath);
                         vrStartupProcess.WaitForExit();
                     }
                     else
-                        MessageBox.Show("Unable to find vrstartup executable within SteamVR installation directory.");
+                        MessageBox.Show("SteamVR does not exist in installation directory.");
                 }
                 else
-                    MessageBox.Show("Couldn't find SteamVR! (Did you install it and run it once?)");
+                    MessageBox.Show("Could not find openvr config file within LocalAppdata.");
             }
             catch (Exception e)
             {
-                MessageBox.Show($"An exception occured while attempting to find SteamVR! (Did you install it and run it once?)\n\nMessage: {e}");
+                MessageBox.Show($"An exception occured while attempting to find SteamVR!\n\nMessage: {e}");
             }
         }
     }
