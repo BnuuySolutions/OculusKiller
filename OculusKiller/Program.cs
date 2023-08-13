69de467750c27a4ca7e5d5887ac16c9b52a0e565
Identifier <1703361+Identifier@users.noreply.github.com>
2023-08-13 22:38:38 +0800

    Waiting for VRMonitor to exit before exiting OculusDash.exe

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -24,6 +24,62 @@ namespace OculusKiller
                     {
                         Process vrStartupProcess = Process.Start(vrStartupPath);
                         vrStartupProcess.WaitForExit();
+
+                        // At this point, vrstartup.exe should have launched vrmonitor.exe.
+                        // This is the one we actually need to wait for to exit when the user clicks "EXIT VR" in the SteamVR dashboard.
+                        var vrMonitorProcesses = Process.GetProcessesByName("vrmonitor");
+                        if (vrMonitorProcesses.Length > 0)
+                        {
+                            foreach (var vrMonitorProcess in vrMonitorProcesses)
+                            {
+                                vrMonitorProcess.WaitForExit();
+                            }
+
+                            // Since exiting SteamVR doesn't actually tell the OVRService to disconnect the headset,
+                            // the OVRService will still immediately re-launch if we exit here.
+                            // Even if we return the same exit code as the official OculusDash.exe
+                            // does when the user presses Quit, which is 0xc0000005, it doesn't stop OVRServer from re-launching us.
+
+#if false // Doesn't work without admin privileges.
+                            // The only way we know of to prevent this is to stop the OVRService service or kill the OVRServer process.
+                            var ovrService = new System.ServiceProcess.ServiceController("OVRService");
+                            ovrService.Stop(); 
+                            ovrService.WaitForStatus(System.ServiceProcess.ServiceControllerStatus.Stopped);
+                            // Restart it so that it's ready for the next time the user wants to connect their headset.
+                            ovrService.Start();
+#endif
+
+#if false // Thwarted by Oculus Client's "Are you sure you want to quit?" dialog.
+                            // Gracefully stop the OculusClient process.
+                            var oculusClientProcesses = Process.GetProcessesByName("OculusClient");
+                            if (oculusClientProcesses.Length > 0)
+                            {
+                                foreach (var oculusClientProcess in oculusClientProcesses)
+                                {
+                                    oculusClientProcess.CloseMainWindow();
+                                }
+                                foreach (var oculusClientProcess in oculusClientProcesses)
+                                {
+                                    oculusClientProcess.WaitForExit();
+                                }
+                            }
+                            else
+                                MessageBox.Show("Could not find OculusClient process.");
+#endif
+
+                            // Last resort: Kill the OVRServer process (might end in _x86 or _x64).
+                            foreach (var ovrServerProcess in Process.GetProcesses().Where(p => p.ProcessName.StartsWith("OVRServer")))
+                            {
+                                ovrServerProcess.Kill();
+                            }
+
+                            // The OVRServiceLauncher will immediately re-launch OVRServer and the OculusClient window,
+                            // which is kind of annoying, but at least it will set our audio/microphone devices back to the default ones.
+                            
+                            // We can safely exit now, since killing the OVRServer process already disconnected the headset.
+                        }
+                        else
+                            MessageBox.Show("Could not find vrmonitor process.");
                     }
                     else
                         MessageBox.Show("SteamVR does not exist in installation directory.");
